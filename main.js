2865b1de1cdb46563a3b6d8d04bc40a05f0521c6
TA2k <tombox2020@gmail.com>
2022-06-22 16:55:21 +0200

    add sort

--- main.js
+++ main.js
@@ -458,8 +458,10 @@ class Withings extends utils.Adapter {
                         if (data.activities) {
                             data.activities.sort((a, b) => a.date.localeCompare(b.date));
                         }
-                        if (element.path === "sleepSummary") {
-                            data.series.sort((a, b) => b.startdate - a.startdate);
+                        if (element.path === "sleepSummary" || element.path === "sleep") {
+                            if (data.series && data.series.sort) {
+                                data.series.sort((a, b) => b.startdate - a.startdate);
+                            }
                         }
                         if (element.path === "sleep") {
                             data.series.map((element) => {
